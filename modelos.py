99b6dc3637598cecfc612b5716f84cdb8eb31b02
Vyctor <vyctorlevy10@gmail.com>
2026-02-19 21:57:00 -0300

    quarta aula

---
+++ modelos.py
@@ -0,0 +1,11 @@
+""""
+Fabrica de chocolate 
+Atributos:  
+fazer pedido / gera um pedido de compra de chocolate 
+tempo / serve para da prioridade a clientes mais velhos 
+importancia / serve para dar a importancia certa para os clientes 
+cpf / para indentificar o cliente 
+nome / para buscar o cliente
+
+"""
+class cliente:
